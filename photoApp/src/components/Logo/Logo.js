51b5e1219f0776d5dff14329c8a6a83525f52ca7
Timofte Serban Gabriel <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2023-05-16 16:13:29 +0300

    Add files via upload

---
+++ photoApp/src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () =>{
+    return(
+        <div className="ma4 mt0" style={{display: 'flex', justifyContent: 'flex-start', padding: '10px'}}>
+            <img src="../img/ebrnet.png" alt="logo" />
+        </div>
+    );
+}
+
+export default Logo;
---
+++ photoApp/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div style={{margin: '75px'}}>
+            <div className="black f3">
+                {'Friend, your rank is...'}
+            </div>
+            <div className="black f3">
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
